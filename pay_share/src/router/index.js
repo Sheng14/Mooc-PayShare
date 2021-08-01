bd8f7ec9bdb0b9dcc9eb1d38ce17f0bab27f5b9c
Sheng14 <1336840941@qq.com>
2021-08-01 11:29:16 +0800

    H5还原UI设计稿

--- pay_share/src/router/index.js
+++ pay_share/src/router/index.js
@@ -1,22 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: () => import('../views/index.vue'),
+    meta:{title: '首页'}
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/activity',
+    name: 'activity',
+    component: () => import('../views/activity.vue'),
+    meta:{title: '详情'}
+  },
+  {
+    path: '/pay',
+    name: 'pay',
+    component: () => import('../views/pay.vue'),
+    meta:{title: '支付'}
+  },
+  {
+    path: '*',
+    component: () => import('../views/index.vue')
   }
 ]
 
